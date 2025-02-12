9885cabeee3dee9eae248b108d941c4d692d5fd3
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-02-12 10:33:32 +0500

    excercise part 2

--- basics/ScalaBasics/src/lectures/scala/Excercise/OOPExcercise.scala
+++ basics/ScalaBasics/src/lectures/scala/Excercise/OOPExcercise.scala
@@ -1,17 +1,24 @@
 package Excercise
 
 object OOPExcercise extends App {
-  val write = new Writer("Ali","Khan",100)
-//  println(write.fullName("Ali","khan"))
+//  val write = new Writer("Ali","Khan",100)
+////  println(write.fullName("Ali","khan"))
+//
+//  val author = new Writer("Charles","Dicken",1812)
+//  val novel  = new Novel("Great Expectations",1861,author)
+//
+//  val author1 = new Writer("Charles","Ali", 1912)
+//  val novel1 = new Novel("1983",1961,author1)
+//
+//  println(novel.isWrittenBy(author))
+//  println(novel.authorAge)
+//
+  val counter = new Counter
+  counter.inc.print // method chaining same like counter inc print   ---> syntactic sugar in sense
+  counter.dec.print
+  counter.dec.print
 
-  val author = new Writer("Charles","Dicken",1812)
-  val novel  = new Novel("Great Expectations",1861,author)
-
-  val author1 = new Writer("Charles","Ali", 1912)
-  val novel1 = new Novel("1983",1961,author1)
-
-  println(novel.isWrittenBy(author))
-  println(novel.authorAge)
+  counter.inc(10).print
 
 }
 
@@ -25,4 +32,26 @@ class Novel(name:String, year:Int, author: Writer) {
   def isWrittenBy(author: Writer) = author == this.author
   def copy(newYear: Int): Novel = new Novel (name,newYear,author)
 
+}
+class Counter(val n:Int =0) {
+  def inc = {
+    println("Incrementing")
+    new Counter(n + 1)
+  }
+
+  def dec = {
+    println("Decrementing")
+    new Counter(n - 1)
+  }
+
+  def inc(n: Int): Counter ={
+  if( n <= 0) this
+  else inc.inc(n - 1)
+  }
+
+  def dec(n:Int): Counter = {
+    if(n <= 0) this
+    else dec.dec(n-1)
+  }
+  def print = println(n)
 }
