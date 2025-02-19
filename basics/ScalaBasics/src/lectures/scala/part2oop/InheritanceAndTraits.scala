29b27b9a44e89ba1da0634d9673006652a560a75
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-02-19 10:13:06 +0500

    inheritance, constructors, overriding

--- basics/ScalaBasics/src/lectures/scala/part2oop/InheritanceAndTraits.scala
+++ basics/ScalaBasics/src/lectures/scala/part2oop/InheritanceAndTraits.scala
@@ -4,18 +4,36 @@ object InheritanceAndTraits extends App {
 
 
   class Animal {
-    def eat = println("hmmmmmm!!!!!!!!I will eat now")
-  }
-  class Cat extends Animal {
 
+    val creatureType = "wild"
+    ///inherit only the nan-private members of the super class
+    protected def eat = println("hmmmmmm!!!!!!!!I will eat you now")
+  }
+  //scala has single class inheritance
+  class Cat extends Animal {
+    def crunch = {
+      eat //protect methods can be called in the child/derived classes
+      println("crunch crunch")
+    }
   }
   val cat = new Cat
-  cat.eat
+  cat.crunch /// I will eat you
 
 
   //CONSTRUCTORS
   class Person(name: String, age:Int) {
-    def this(name:String) = this(name,0)
+    def this() = this("Atif",0)
   }
-  class Adult(name:String, age: Int, idCard: String) extends Person(name)
+  class Adult(name:String, age: Int, idCard: String) extends Person() //This is the correct way of extending a class with perameters
+ class Dog(override val creatureType:String) extends Animal {
+//    //if this is not available the object will call the val from the parent class
+   override def eat = println("crunch crunch")
+ }
+ val dog = new Dog("K9") //this is overriding the vals directly in the class constructor...
+ dog.eat
+ println(dog.creatureType)
+
+ //
+
+
 }
