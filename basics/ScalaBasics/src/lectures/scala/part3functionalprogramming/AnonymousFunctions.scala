cce4cb6409a7e96add64e3e7806f05a202cbbd3c
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-02-26 13:55:23 +0500

    anonymous function syntax practice

--- basics/ScalaBasics/src/lectures/scala/part3functionalprogramming/AnonymousFunctions.scala
+++ basics/ScalaBasics/src/lectures/scala/part3functionalprogramming/AnonymousFunctions.scala
@@ -1,13 +1,13 @@
 package part3functionalprogramming
 
 object AnonymousFunctions extends App {
-  //The Object Oriented way of defining a function and instantiating it on the spot
+  //The Object-Oriented way of defining a function and instantiating it on the spot
   val doublar = new Function1[Int,Int] {
     override def apply(v1: Int): Int = v1 * 2
   }
   //Anonymous Function or (LAMBDA)
   val doublar2 = (x:Int) => x * 2
-  //more shorter syntax
+  //shorter syntax
   val doublar00: Int => Int = x => x * 2
 //  println(doublar2(2))
 
@@ -26,4 +26,13 @@ object AnonymousFunctions extends App {
     str.toInt
   }
   println(stringtoInt("12") + 12)
+
+  val takeInt = new Function1[Int, Function1[Int, Int]] {
+    override def apply(v1: Int): Function1[Int, Int] = new Function1[Int, Int] {
+      override def apply(v2: Int): Int = v1 + v2
+    }
+  }
+  val takeIn = (x:Int) => (y:Int) => x + y
+  println(takeInt(12)(11))
+
 }
