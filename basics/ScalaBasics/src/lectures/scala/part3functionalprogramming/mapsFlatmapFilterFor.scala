b2b4950db82d57d7f03385317bb67f52e170720e
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-02-28 11:33:09 +0500

    filters and flatMap in Scala

--- basics/ScalaBasics/src/lectures/scala/part3functionalprogramming/mapsFlatmapFilterFor.scala
+++ basics/ScalaBasics/src/lectures/scala/part3functionalprogramming/mapsFlatmapFilterFor.scala
@@ -16,10 +16,39 @@ object mapsFlatmapFilterFor extends App {
 
   //map on String values
   //strings are sequences of characters, map can be applied
-
   val str = "Hello"
   val str2= "World"
   val upperCase = str.map(str => str.toUpper)
   val upperCase2= str2.map(_.toUpper) // shorter syntax
   println(s"$upperCase $upperCase2")
+
+
+  //Filter method in Scala
+  //The filter method is used to select elements from a collection(list) that satisfy a given condition.
+
+  val numbers = List(2,4,6,8,10)
+
+  val evenNum = numbers.filter(x => x % 2 == 0)
+  println(evenNum) //returns all the numbers which satisfy the condition for the filter method
+
+  val v = "eeelo"
+  val vowel = v.filter(c => "aeiou".contains(c))
+  println(vowel)
+
+  //flatMap
+  //usefull when working with nested structures
+  //flatMap applies function to each elements( like map does)
+  ///Flatten: removes extra layar of nesting
+  //it applies a functions and flatten the result into a single collection(list)
+
+  //using map
+  val nums = List(1,2,3)
+  val mapped = nums.map(x => List(x,x * 2))
+  println(mapped) //output:::List(List(1, 2), List(2, 4), List(3, 6)) returned a nested list
+  ///using flatMap
+  val flatMapped = nums.flatMap(x => List(x,x*2))
+  println(flatMapped) //output:::List(1, 2, 2, 4, 3, 6) returned a single list
+
+
+
 }
